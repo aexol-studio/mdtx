5c5f0e9f6aafa1fa6cb7fc24f4aaa971c588c6f5
Aleksander <aleksander.bondar@aexol.com>
2022-10-18 15:51:54 +0200

    addedcoverttohtmlmarkdown

--- packages/mdtx-core/src/transformers/markdown.ts
+++ packages/mdtx-core/src/transformers/markdown.ts
@@ -4,6 +4,14 @@ import { pathIn, pathOut } from '@/paths.js';
 import matter from 'gray-matter';
 import fs from 'fs';
 import { ConfigFile } from '@/config.js';
+import remarkRehype from 'remark-rehype';
+import remarkParse from 'remark-parse';
+import rehypeRaw from 'rehype-raw';
+import rehypeStringify from 'rehype-stringify';
+import remarkGfm from 'remark-gfm'
+import rehypeHighlight from 'rehype-highlight';
+import { unified } from 'unified';
+
 
 export const transformMarkdownFiles =
   (config: ConfigFile) => async (mdFiles: string[]) => {
@@ -15,8 +23,19 @@ export const transformMarkdownFiles =
       await Promise.all(
         mdFiles.map(async (mdFile) => {
           const m = matter(await fs.promises.readFile(pathIn(config)(mdFile)));
+
+          const processedContent = await unified()
+            .use(remarkParse)
+            .use(remarkRehype, { allowDangerousHtml: true })
+            .use(rehypeRaw)
+            .use(remarkGfm)
+            .use(rehypeHighlight)
+            .use(rehypeStringify)
+            .process(m.content)
+
+          const contentHtml = processedContent.toString();
           generatedMdLib[mdFile] = {
-            content: m.content,
+            content: config.markdownToHtml ? contentHtml : m.content,
             data: m.data,
             excerpt: m.excerpt,
           };
