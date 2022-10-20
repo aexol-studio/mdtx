450712237e70a9aa22d4cbeace3b7025276427de
Aleksander Bondar <darkduble12@gmail.com>
2022-10-20 11:19:23 +0200

    some plugin improvements

--- packages/mdtx-plugin-nextjs/index.ts
+++ packages/mdtx-plugin-nextjs/index.ts
@@ -1,28 +1,25 @@
-
 const withMDtx =
-  (configMDtx: { out: string; in: string; markdownToHtml: boolean; }) =>
-    (nextConfig: { webpack: (config: any, options: any) => any }) => {
-      return Object.assign({}, nextConfig, {
-        webpack(
-          config: { plugins: any[] },
-          options: { dev: boolean; isServer: boolean },
-        ) {
-          if (options.dev && options.isServer) {
-            // eslint-disable-next-line @typescript-eslint/no-var-requires
-            const WebpackCustomWithMDtxPlugin = require('./WebpackCustomWithMDtxPlugin');
-            config.plugins.push(new WebpackCustomWithMDtxPlugin(configMDtx));
+  (configMDtx: { out: string; in: string; markdownToHtml: boolean }) =>
+  (nextConfig: { webpack: (config: any, options: any) => any }) => {
+    return Object.assign({}, nextConfig, {
+      webpack(
+        config: { plugins: any[] },
+        options: { dev: boolean; isServer: boolean },
+      ) {
+        if (options.dev && !options.isServer) {
+          // eslint-disable-next-line @typescript-eslint/no-var-requires
+          const WebpackCustomWithMDtxPlugin = require('./WebpackCustomWithMDtxPlugin');
+          config.plugins.push(new WebpackCustomWithMDtxPlugin(configMDtx));
 
-            if (typeof nextConfig.webpack === 'function') {
-              return nextConfig.webpack(config, options);
-            }
-            return config;
-          } else {
-            return config;
+          if (typeof nextConfig.webpack === 'function') {
+            return nextConfig.webpack(config, options);
           }
-
-
-        },
-      });
-    };
+          return config;
+        } else {
+          return config;
+        }
+      },
+    });
+  };
 
 module.exports = withMDtx;
