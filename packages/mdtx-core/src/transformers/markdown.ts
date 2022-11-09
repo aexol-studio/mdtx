85a3d866a7f560f69dccd4313e96751d9a30d13b
aexol <aexol@aexol.com>
2022-11-09 14:37:46 +0100

    :sparkles: recuirsive md tree

--- packages/mdtx-core/src/transformers/markdown.ts
+++ packages/mdtx-core/src/transformers/markdown.ts
@@ -14,98 +14,82 @@ import rehypeHighlight from 'rehype-highlight';
 import { unified, Processor } from 'unified';
 import remarkDirective from 'remark-directive';
 
-type Plugins = {
-  markdown?: (uni: Processor) => Processor;
-  directive?: (uni: Processor) => Processor;
-  custom?: (uni: Processor) => Processor;
-  rehype?: (uni: Processor) => Processor;
-  raw?: (uni: Processor) => Processor;
-  gfm?: (uni: Processor) => Processor;
-  highlight?: (uni: Processor) => Processor;
-  stringify?: (uni: Processor) => Processor;
+const baseTree = unified().use(remarkParse);
+
+const remarkDirectiveTree = (t: typeof baseTree, r: typeof remarkDirective) =>
+  t.use(r);
+
+const remarkRehypeTree = (
+  t: ReturnType<typeof remarkDirectiveTree>,
+  r: typeof remarkRehype,
+) =>
+  t.use(r, {
+    allowDangerousHtml: true,
+  });
+
+const remarkRawTree = (
+  t: ReturnType<typeof remarkRehypeTree>,
+  r: typeof rehypeRaw,
+) => t.use(r);
+
+const remarkGfmTree = (
+  t: ReturnType<typeof remarkRawTree>,
+  r: typeof remarkGfm,
+) => t.use(r);
+
+const finalTree = (
+  t: ReturnType<typeof remarkGfmTree>,
+  r: typeof rehypeHighlight,
+) => t.use(r);
+
+export const buildTree = (options?: {
+  t1TreeDirective?: typeof remarkDirectiveTree;
+  t2TreeRehype?: typeof remarkRehypeTree;
+  t3TreeRaw?: typeof remarkRawTree;
+  t4TreeGfm?: typeof remarkGfmTree;
+  t5TreeFinal?: typeof finalTree;
+}) => {
+  const {
+    t1TreeDirective = remarkDirectiveTree,
+    t2TreeRehype = remarkRehypeTree,
+    t3TreeRaw = remarkRawTree,
+    t4TreeGfm = remarkGfmTree,
+    t5TreeFinal = finalTree,
+  } = options || {};
+  return t5TreeFinal(
+    t4TreeGfm(
+      t3TreeRaw(
+        t2TreeRehype(t1TreeDirective(baseTree, remarkDirective), remarkRehype),
+        rehypeRaw,
+      ),
+      remarkGfm,
+    ),
+    rehypeHighlight,
+  );
 };
 
-const convertToHtml = async (
-  m: matter.GrayMatterFile<Buffer>,
-  plugins?: Plugins,
+export const baseParserTree = buildTree();
+
+export const stringifyUnifiedProcessor = <T extends Processor>(
+  p: T,
+  content: string,
 ) => {
-  const processUni = (k: keyof Plugins, p: Processor) => plugins?.[k]?.(p) || p;
-
-  const markdownContent = processUni('markdown', unified().use(remarkParse));
-
-  const rehypeContent = processUni(
-    'rehype',
-    markdownContent.use(remarkRehype, { allowDangerousHtml: true }),
-  );
-
-  const rawContent = processUni('raw', rehypeContent.use(rehypeRaw));
-
-  const gfmContent = processUni('gfm', rawContent.use(remarkGfm));
-
-  const highlightContent = processUni(
-    'highlight',
-    gfmContent.use(rehypeHighlight),
-  );
-
-  const stringifyContent = processUni(
-    'stringify',
-    highlightContent.use(rehypeStringify),
-  );
-
-  const processedContent = await stringifyContent.process(m.content);
-
-  return processedContent.toString();
+  return p
+    .use(rehypeStringify)
+    .process(content)
+    .then((r) => r.toString());
 };
 
-export const convertMarkdownToHtml = async (
-  mdString: string,
-  customPlugin?: Function,
-  plugins?: Plugins,
-) => {
-  const processUni = (k: keyof Plugins, p: Processor) => plugins?.[k]?.(p) || p;
+const convertToHtml = async (m: matter.GrayMatterFile<Buffer>) => {
+  return stringifyUnifiedProcessor(baseParserTree, m.content);
+};
 
-  const markdownContent = processUni('markdown', unified().use(remarkParse));
-
-  const directiveContent = processUni(
-    'directive',
-    markdownContent.use(remarkDirective),
-  );
-
-  const customContent = processUni(
-    'custom',
-    directiveContent.use(() => {
-      return (tree: any) => {
-        customPlugin?.(tree);
-      };
-    }),
-  );
-
-  const rehypeContent = processUni(
-    'rehype',
-    customContent.use(remarkRehype, { allowDangerousHtml: true }),
-  );
-
-  const rawContent = processUni('raw', rehypeContent.use(rehypeRaw));
-
-  const gfmContent = processUni('gfm', rawContent.use(remarkGfm));
-
-  const highlightContent = processUni(
-    'highlight',
-    gfmContent.use(rehypeHighlight),
-  );
-
-  const stringifyContent = processUni(
-    'stringify',
-    highlightContent.use(rehypeStringify),
-  );
-
-  const processedContent = await stringifyContent.process(mdString);
-
-  return processedContent.toString();
+export const convertMarkdownToHtml = async (mdString: string) => {
+  return stringifyUnifiedProcessor(baseParserTree, mdString);
 };
 
 export const transformMarkdownFiles =
-  (config: ConfigFile) => async (mdFiles: string[], plugins?: Plugins) => {
+  (config: ConfigFile) => async (mdFiles: string[]) => {
     try {
       const generatedMdLib: generatedMdLibType = {};
       await Promise.all(
@@ -113,9 +97,7 @@ export const transformMarkdownFiles =
           const m = matter(await fs.promises.readFile(pathIn(config)(mdFile)));
 
           generatedMdLib[mdFile] = {
-            content: config.markdownToHtml
-              ? await convertToHtml(m, plugins)
-              : m.content,
+            content: config.markdownToHtml ? await convertToHtml(m) : m.content,
             data: m.data,
             excerpt: m.excerpt,
           };
--- sandbox/mdtx-nextjs-example/utils/transformers.ts
+++ sandbox/mdtx-nextjs-example/utils/transformers.ts
@@ -1,7 +1,12 @@
-import { convertMarkdownToHtml } from 'mdtx-core';
+import {
+  baseParserTree,
+  buildTree,
+  stringifyUnifiedProcessor,
+} from 'mdtx-core';
 import { htmlContent } from '../content/docs';
 import { visit } from 'unist-util-visit';
 import { h } from 'hastscript';
+import { Root } from 'mdast';
 
 const replaceCustomTagWithMarkdownDirective =
   (htmlContent: string) => (customTag: string) => {
@@ -12,31 +17,7 @@ const replaceCustomTagWithMarkdownDirective =
       .replace(endRegex, `:::`);
   };
 
-const renameImgSrcs = (tree: any) => {
-  visit(tree, 'containerDirective', (node) => {
-    const data = node.data || (node.data = {});
-    const hast = h(node.name, node.attributes) as unknown as {
-      tagName: string;
-      properties: string[];
-    };
-
-    data.hName = hast.tagName;
-    data.hProperties = hast.properties;
-  });
-  visit(tree, 'html', (node) => {
-    if (node.value.includes('src="')) {
-      node.value = node.value.replace(/src\=\".*\.gitbook/, 'src="');
-      // tree.children.push({ type: '' });
-    }
-  });
-  visit(tree, 'paragraph', (node) => {
-    const image = node.children.find((child: any) => child.type === 'image');
-    if (image && image.type === 'image') {
-      const fileName = image.url.replace(/^.*\.gitbook/, '');
-      image.url = fileName;
-    }
-  });
-};
+const renameImgSrcs = (tree: any) => {};
 
 export const getHTMLfile = async () => {
   const content = htmlContent['graphql-editor/graph/create.md'].content;
@@ -44,5 +25,37 @@ export const getHTMLfile = async () => {
   const processedContent =
     replaceCustomTagWithMarkdownDirective(content)('hint');
 
-  return await convertMarkdownToHtml(processedContent, renameImgSrcs);
+  const myTree = buildTree({
+    t1TreeDirective: (t, r) => {
+      return t.use(r).use(() => {
+        return (n: Root) => {
+          visit(n, 'containerDirective', (node) => {
+            const data = node.data || (node.data = {});
+            const hast = h(node.name, node.attributes) as unknown as {
+              tagName: string;
+              properties: string[];
+            };
+
+            data.hName = hast.tagName;
+            data.hProperties = hast.properties;
+          });
+          visit(n, 'html', (node) => {
+            if (node.value.includes('src="')) {
+              node.value = node.value.replace(/src\=\".*\.gitbook/, 'src="');
+              // tree.children.push({ type: '' });
+            }
+          });
+          visit(n, 'paragraph', (node) => {
+            const image = node.children.find((child) => child.type === 'image');
+            if (image && image.type === 'image') {
+              const fileName = image.url.replace(/^.*\.gitbook/, '');
+              image.url = fileName;
+            }
+          });
+        };
+      });
+    },
+  });
+
+  return stringifyUnifiedProcessor(myTree, processedContent);
 };
