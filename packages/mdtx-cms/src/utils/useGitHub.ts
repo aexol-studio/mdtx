0cbe6a380a1ed3db5f4810732124bace62c41f44
Aleksander <darkduble12@gmail.com>
2022-12-12 19:56:32 +0100

    dev envs

--- packages/mdtx-cms/src/utils/useGitHub.ts
+++ packages/mdtx-cms/src/utils/useGitHub.ts
@@ -7,7 +7,7 @@ export const useGitHub = () => {
   const octokit = new Octokit({
     auth: token,
   });
-
+  const connectURL = process.env.NEXT_PUBLIC_PROXY || 'http://localhost:7071';
   //USE: FOR SEARCH FOR REPOSITORIES
   const getGitHubSearchRepositories = async (
     q: string,
@@ -33,14 +33,9 @@ export const useGitHub = () => {
     const { data } = await octokit.request(`GET ${url.pathname}`, {
       request: {
         fetch: async (url: string, opts: RequestInit | undefined) =>
-          fetch(
-            `https://mdtx-proxy-github-dev.azurewebsites.net/api${
-              new URL(url).pathname
-            }`,
-            {
-              ...opts,
-            },
-          ),
+          fetch(`${connectURL}/api${new URL(url).pathname}`, {
+            ...opts,
+          }),
       },
     });
     const fileArray = await unzipFunction(data);
@@ -52,15 +47,12 @@ export const useGitHub = () => {
   };
   //USE: FOR AUTHENTICATE
   const getGitHubToken = async (code: string) => {
-    const response = await fetch(
-      `https://mdtx-proxy-github-dev.azurewebsites.net/authenticate/${code}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+    const response = await fetch(`${connectURL}/authenticate/${code}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
       },
-    ).catch(() => {});
+    }).catch(() => {});
     if (!response) throw new Error('Bad response from getGitHubToken()');
     return await response.json();
   };
